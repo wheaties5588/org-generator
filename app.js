ad294e6292b2356c4236c65f941eaf158aa1ec70
Matt Wieciech <matt.wieciech@gmail.com>
2020-07-25 15:09:33 -0400

    Set up question blocks and functions containing inquirer prompts

--- app.js
+++ app.js
@@ -14,51 +14,180 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-function newEmployeePrompt() {
+
+const managerQuestions = [
+    {
+        type: "input",
+        message: "What is the name of the manager? ",
+        name: "managerName"
+    },
+    {
+        type: "input",
+        message: "What is the ID number of the manager? ",
+        name: "managerID"
+    },
+    {
+      type: "input",
+      message: "What is the email address of the manager? ",
+      name: "managerEmail"
+    },
+    {
+      type: "input",
+      message: "What is the office number of the manager? ",
+      name: "managerOffice"
+    }
+]
+
+
+const engineerQuestions = [
+  {
+      type: "input",
+      message: "What is the name of the engineer? ",
+      name: "engName"
+  },
+  {
+      type: "input",
+      message: "What is the ID number of the engineer? ",
+      name: "engID"
+  },
+  {
+    type: "input",
+    message: "What is the email address of the engineer? ",
+    name: "engEmail"
+  },
+  {
+    type: "input",
+    message: "What is the github username of the engineer? ",
+    name: "engGithub"
+  }
+]
+
+
+const internQuestions = [
+  {
+      type: "input",
+      message: "What is the name of the intern? ",
+      name: "internName"
+  },
+  {
+      type: "input",
+      message: "What is the ID number of the intern? ",
+      name: "internID"
+  },
+  {
+    type: "input",
+    message: "What is the email address of the intern? ",
+    name: "internEmail"
+  },
+  {
+    type: "input",
+    message: "What school does the intern currently attend? ",
+    name: "internSchool"
+  }
+]
+
+
+var employees = [];
+
+//New Manager
+newManager = () => {
+  
+  inquirer.prompt(managerQuestions).then(res => {
     
-inquirer
-    .prompt([
-        {
-            type: "list",
-            message: "What type of employee would you like to create?",
-            name: "newEmployee",
-            choices: [
-                "Manager",
-                "Engineer",
-                "Intern",
-                "Done creating employees"
-            ]
-            
-        }
-    ])
-    .then(answers => {
-            testFunction(answers.newEmployee);
-        
-    })
-    .catch(error => {
-        if(error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-        } else {
-        // Something else when wrong
-        }
-    });
+    employees.push(new Manager(res.managerName, res.managerID, res.managerEmail, res.managerOffice));
+    newEmp();
     
+  });
 }
 
-newEmployeePrompt();
-
-function testFunction(emp) {
-    if (emp !== "Done creating employees") {
-        newEmployeePrompt();
+//New engineer or intern
+newEmp = () => {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What type of employee would you like to create? ",
+      name: "empType",
+      choices: [
+        "Engineer",
+        "Intern",
+        "Done creating employees"
+      ]
     }
-};
+  ]).then(res => {
+    if (res.empType === "Engineer") {
+      newEngineer();
+    } else if (res.empType === "Intern") {
+      newIntern();
+    } else {
+      // Write to file
+      fs.writeFile(outputPath, render(employees), "utf8", function() {
+        console.log("File successfully created!");
+      })
+    }
+  }).catch(error => {
+    console.log("Could not create file.");
+    console.log(error);
+  });  
+}
+
+newEngineer = () => {
+  inquirer.prompt(engineerQuestions).then(res => {
+    employees.push(new Engineer(res.engName, res.engID, res.engEmail, res.engGithub));
+    newEmp();
+  })
+}
 
 
-// if (newRole !== "Done creating employees") {
-//     console.log(newRole);
-//     newEmployeePrompt();
+newIntern = () => {
+  inquirer.prompt(internQuestions).then(res => {
+    employees.push(new Intern(res.internName, res.internID, res.internEmail, res.internSchool));
+    newEmp();
+  })
+}
+
+
+//Call manager function to start asking questions
+newManager();
+
+
+// const createEmployees = async () => {
+    
+//         const { another, ...empInfo } = await inquirer.prompt(managerQuestions);
+//         employees.push(empInfo);
+        
+//         if (another) {
+//             createEmployees()
+//         } else {
+//             console.log(employees);
+//             return employees;
+//         }
 // }
 
+// createEmployees();
+
+// const blah = () => {
+//     inquirer
+//   .prompt(managerQuestions)
+//   .then((res) => {
+//       employees.push(res);
+//       console.log(employees);
+//       console.log(res[0].another);
+      
+//       if (res[0].another) {
+//           blah();
+//       }
+//   })
+//   .catch(error => {
+//     if(error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else when wrong
+//     }
+//   });
+  
+// }
+
+// blah();
 
 
 // After the user has input all employees desired, call the `render` function (required
